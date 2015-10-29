c41343bfb75610dae875383e60d8f58bb9a73fee
David Hoepelman <dhoepelman@gmail.com>
2015-10-29 18:09:28 +0100

    Another try

--- src/XLParser.DebugVisualizer/Visualizer.cs
+++ src/XLParser.DebugVisualizer/Visualizer.cs
@@ -4,26 +4,27 @@ using Irony.Parsing;
 //using XLParser;
 
 // Compiling this file and placing the resulting DLL in My Documents\Visual Studio 20XX\Visualizers will improve what the VS debugger displays for parse trees
-[assembly: DebuggerTypeProxy(typeof(XLParser.DebugVisualizer.VisualizerPrint), Target = typeof(Irony.Parsing.ParseTreeNode))]
+[assembly: DebuggerTypeProxy(typeof(VisualizerPrint), Target = typeof(Irony.Parsing.ParseTreeNode))]
 
-namespace XLParser.DebugVisualizer
+public class VisualizerPrint
 {
-    public class VisualizerPrint
+    public ParseTreeNode _base { get; }
+
+    public VisualizerPrint(ParseTreeNode node)
     {
-        public ParseTreeNode _base { get; }
-
-        public VisualizerPrint(ParseTreeNode node)
-        {
-            _base = node;
-        }
-
-        // Native properties
-        public ParseTreeNodeList ChildNodes => _base.ChildNodes;
-        public string Token => _base.Token?.Text ?? "";
-
-        // Computed properties
-        //public string Print => _base.Print();
-        //public string Type => _base.Type();
-        //public ParseTreeNode RelevantChildNode => _base.SkipToRelevant();
+        _base = node;
     }
+
+    // Native properties
+    public ParseTreeNodeList ChildNodes => _base.ChildNodes;
+    public string Token => _base.Token?.Text ?? "";
+
+    public readonly string Blah = "This is a test";
+
+    public override string ToString() => "This is another test";
+
+    // Computed properties
+    //public string Print => _base.Print();
+    //public string Type => _base.Type();
+    //public ParseTreeNode RelevantChildNode => _base.SkipToRelevant();
 }
